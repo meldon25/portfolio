fab5a7003855069d4f2cd2a12b322be16a296ad4
Melissa Donegan <melissadonegan25@gmail.com>
2020-01-27 14:48:10 -0500

    update css

--- portfolio/src/shared/Work.jsx
+++ portfolio/src/shared/Work.jsx
@@ -24,19 +24,29 @@ class Work extends Component {
 							<p>Languages used:</p>
 							<p>React</p>
 							<p>Ruby on Rails</p>
-							<button className="link-button">Live Link</button>
-							<button className="github-button">Github</button>
+							<div className="button-cont">
+							<button className="link-button" disabled='disabled'>Stay Tuned</button>
+							<form action="https://github.com/meldon25/project-5">
+								<button className="github-button" type="submit">Github</button>
+							</form>
+							</div>
 						</div>
 					</div>
 					<div className="project-2">
-					<div className="project-2-desc">
-						<h1>In A Pinch</h1>
+						<div className="project-2-desc">
+							<h1>In A Pinch</h1>
 							<p>In A Pinch is a React app that was a collaboration with UX designers. We adhered to a Zeplin design and built out functionality for the app.</p>
 							<p>Languages used:</p>
 							<p>React</p>
 							<p>CSS</p>
+							<div className="button-cont">
+							<form action="https://inapinch.surge.sh">
 							<button className="link-button">Live Link</button>
-							<button className="github-button">Github</button>
+							</form>
+							<form action="https://github.com/meldon25/p4-colab">
+								<button className="github-button" type="submit">Github</button>
+							</form>
+							</div>
 						</div>
 						<div className="project-2-img">
 							<img src="https://i.imgur.com/4d4lqWo.png" />
@@ -52,8 +62,14 @@ class Work extends Component {
 							<p>Languages used:</p>
 							<p>React</p>
 							<p>API</p>
-							<button className="link-button">Live Link</button>
-							<button className="github-button">Github</button>
+							<div className="button-cont">
+							<form action="https://etoile.surge.sh">
+								<button className="link-button" type="submit">Website</button>
+							</form>
+							<form action="https://github.com/meldon25/p-two">
+								<button className="github-button" type="submit">Github</button>
+							</form>
+							</div>
 						</div>
 					</div>
 				</div>
