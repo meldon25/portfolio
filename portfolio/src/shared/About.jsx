a7324e04673c2b860317a0c5714534a17db9b336
Melissa Donegan <melissadonegan25@gmail.com>
2020-02-05 19:17:17 -0500

    respo

--- portfolio/src/shared/About.jsx
+++ portfolio/src/shared/About.jsx
@@ -2,25 +2,25 @@ import React from 'react'
 import '../styles/about.css'
 
 function About() {
-    return (
-        <div id="About">
-            <div className="about-cont">
-            <h1 className='about-tab'><p className="contnum">2.</p>About<div className="line-2"></div></h1>
-            <div className='about-container'>
-                <div className="left-side">
-                    <p className='about-desc'><h2>What I Do</h2>I am a driven and creative problem-solver with an eye for design and a commitment to putting the client’s needs first. I grow best when I am constantly challenged to learn new things and I hold myself accountable to staying ahead of the curve. I take pride in building projects collaboratively through strong communication and through targeted goal setting.</p>
+  return (
+    <div id="About">
+      <div className="about-cont">
+        <h1 className='about-tab'><p className="contnum">2.</p>About<div className="line-2"></div></h1>
+        <div className='about-container'>
+          <div className="left-side">
+            <p className='about-desc'><h2>What I Do</h2>I am a driven and creative problem-solver with an eye for design and a commitment to putting the client’s needs first. I grow best when I am constantly challenged to learn new things and I hold myself accountable to staying ahead of the curve. I take pride in building projects collaboratively through strong communication and through targeted goal setting.</p>
 
-                </div>
-                <div className="right-side">
-                    <div className='box'></div>
-                    <div className="photo-box">
-                        <img className="photo-box2" src="https://i.imgur.com/QVCDuvl.jpg" />
-                    </div>
-                </div>
+          </div>
+          <div className="right-side">
+            <div className='box'></div>
+            <div className="photo-box">
+              <img className="photo-box2" src="https://i.imgur.com/QVCDuvl.jpg" />
             </div>
-            </div>
+          </div>
         </div>
+      </div>
+    </div>
 
-    )
+  )
 }
 export default About 
