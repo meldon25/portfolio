c4cc9b504a31748db861d6dbc48621ea996489d3
Melissa Donegan <melissadonegan25@gmail.com>
2020-07-28 15:25:54 -0400

    added contact

--- portfolio/client/src/shared/Contact.jsx
+++ portfolio/client/src/shared/Contact.jsx
@@ -7,11 +7,11 @@ class Contact extends Component {
     render() {
         return (
             <div id="Contact">
-                <div className="fullscreen-bg">
-    <video loop muted autoPlay className="fullscreen-bg_video">
-        <source autoPlay loop src="https://i.imgur.com/ZlqeaEO.mp4" type="video/mp4" />
-    </video>
-</div>
+                <div className="background">
+    {/* <video loop muted autoPlay className="fullscreen-bg_video"> */}
+        {/* <source autoPlay loop src="https://i.imgur.com/ZlqeaEO.mp4" type="video/mp4" /> */}
+    {/* </video> */}
+
 <div className="contact-text">
             <div className="contact-cont">
                 <h2 className="contact-title">Contact</h2>
@@ -20,13 +20,17 @@ class Contact extends Component {
             <div className="contact-info">
                 <h1 className='contact-git'>Get In Touch</h1>
                 <p className='contact-email'>melissadonegan25@gmail.com</p>
-                <p className="contact-desc">I'm currently looking for new opportunities! <br /> If you're interested in working together or just want to say hello, send me a message and I will get to your email ASAP!</p>  
                 </div>
-            {/* <div className="contact-box"> */}
-            {/* <input type="hidden" name="form-name" value="contact" /> */}
-                {/* <ContactForm /> */}
+                
+            <div className="contact-box">
+            <input type="hidden" name="form-name" value="contact" />
+            <ContactForm />
+                </div>
+                </div> 
+                </div>
+                {/* <div className="contact-desc-box"> */}
+                {/* <p className="contact-desc">I'm currently looking for new opportunities! <br /> If you're interested in working together or just want to say hello, send me a message and I will get to your email ASAP!</p>  */}
                 {/* </div> */}
-                </div>
                 </div>
             </div>
         )
