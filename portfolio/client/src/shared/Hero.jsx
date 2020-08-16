dcf5e4229a47f6aa9a3c8ef53044d1ca7b0aa50a
Melissa Donegan <melissadonegan25@gmail.com>
2020-08-16 17:03:49 -0400

    changed hero photo resp.

--- portfolio/client/src/shared/Hero.jsx
+++ portfolio/client/src/shared/Hero.jsx
@@ -11,7 +11,7 @@ class Hero extends Component {
         <source autoPlay loop src="https://i.imgur.com/EuPkc7A.mp4" type="video/mp4" />
     </video>
 </div>
-<img className="mobile-photo" src="https://i.imgur.com/BbAN5hx.jpg?1"/>
+<img className="mobile-photo" src="https://i.imgur.com/K7f6QLP.jpg"/>
       <div className="container">
         <h1 className="name-title">Melissa Donegan</h1>
         <p className="hero-desc">Designer and Software Engineer</p>
