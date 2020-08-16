5aa2fd50fe14015b0b600e037405c1713f84e01f
Melissa Donegan <melissadonegan25@gmail.com>
2020-08-16 15:56:31 -0400

    added resp. to lang

--- portfolio/client/src/shared/Hero.jsx
+++ portfolio/client/src/shared/Hero.jsx
@@ -8,10 +8,10 @@ class Hero extends Component {
       <div className="hero-cont">
         <div className="fullscreen-bg">
     <video loop muted autoPlay className="fullscreen-bg_video">
-        {/* <source autoplay loop src="https://i.imgur.com/fSRvq2S.mp4" type="video/mp4" /> */}
         <source autoPlay loop src="https://i.imgur.com/EuPkc7A.mp4" type="video/mp4" />
     </video>
 </div>
+<img className="mobile-photo" src="https://i.imgur.com/BbAN5hx.jpg?1"/>
       <div className="container">
         <h1 className="name-title">Melissa Donegan</h1>
         <p className="hero-desc">Designer and Software Engineer</p>
