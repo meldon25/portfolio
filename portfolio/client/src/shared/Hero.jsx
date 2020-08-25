cc5f44d74bec343048cd3f79cfb35325a449b957
Melissa Donegan <melissadonegan25@gmail.com>
2020-08-25 18:04:50 -0400

    added hero resp. to video

--- portfolio/client/src/shared/Hero.jsx
+++ portfolio/client/src/shared/Hero.jsx
@@ -6,15 +6,16 @@ class Hero extends Component {
   render() {
     return (
       <div className="hero-cont">
-        <div className="fullscreen-bg">
-    <video loop muted autoPlay className="fullscreen-bg_video">
-        {/* <source autoPlay loop src="https://i.imgur.com/EuPkc7A.mp4" type="video/mp4" /> */}
+        <div className="fullscreen-bg embed-responsive embed-repsonsive-16by9">
+    {/* <video loop muted autoPlay className="fullscreen-bg_video">
+        <source autoPlay loop src="https://i.imgur.com/EuPkc7A.mp4" type="video/mp4" />
         <source autoPlay loop src="https://i.imgur.com/oHux9Z2.mp4" type="video/mp4" />
-
-    </video>
+    </video> */}
+    <video className="fullscreen-bg_video" playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
+    <source src="https://i.imgur.com/oHux9Z2.mp4" type="video/mp4" />
+  </video>
+    </div>
     <img className="mobile-photo" src="https://i.imgur.com/K7f6QLP.jpg"/>
-</div>
-
       <div className="container">
         <h1 className="name-title">Melissa Donegan</h1>
         <p className="hero-desc">Designer and Software Engineer</p>
@@ -23,7 +24,8 @@ class Hero extends Component {
     <div className="arrow"></div>
     </div>
     </div>
-    
+    // </div>
+  
       
     )
   }
