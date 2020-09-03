766e9b85c67b426a2b95e21ed8fae593688529e0
Melissa Donegan <melissadonegan25@gmail.com>
2020-09-03 12:52:00 -0400

    added resp. lang.

--- portfolio/client/src/shared/Languages.jsx
+++ portfolio/client/src/shared/Languages.jsx
@@ -13,19 +13,20 @@ class Languages extends Component {
                    <li>HTML5</li>
                    <li>CSS/Sass</li> 
                    <li>JavaScript</li>
-                   <li>React</li>
+                   <li>jQuery</li>
+                   
                </ul>
                <ul className='center-skills'>
-               <li>Node.js</li>
+               <li>React</li>
                <li>PostgreSQL & SQL</li>
                 <li>Ruby</li>
                 <li>Ruby on Rails</li>
                </ul>
                <ul className='right-skills'>
+                   <li>Node.js</li>
                     <li>Express</li>
                     <li>Wordpress</li>
-                    <li>Responsive Design</li>
-                    <li>Figma</li>
+                    <li>Shopify</li>
                </ul>
             </div>
             </div>
